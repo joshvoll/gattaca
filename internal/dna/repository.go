f5cc0f5a3c2e2e92cc13a48e85f28798ace6896e
Josue Rodriguez <joshvoll@yahoo.com>
2020-09-24 12:54:46 -0600

    adding insert and select user from dna

--- internal/dna/repository.go
+++ internal/dna/repository.go
@@ -1,6 +1,7 @@
 package dna
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
@@ -39,4 +40,24 @@ func NewSQLiteRepository(urn string) (*SQLiteRepository, error) {
 	return &SQLiteRepository{
 		db: db,
 	}, nil
+}
+
+// Insert inser a user with the dna sequence and return and error is ocurrs
+func (s *SQLiteRepository) Insert(ctx context.Context, user, sequence string) error {
+	_, err := s.db.ExecContext(ctx, `INSERT INTO dna (user, sequence) VALUES (?,?)`, user, sequence)
+	if err != nil {
+		return fmt.Errorf("error writing to the repository")
+	}
+
+	return nil
+}
+
+// Select a user dna sequence from the database
+func (s *SQLiteRepository) Select(ctx context.Context, user string) (sequence string, err error) {
+	if err := s.db.QueryRowContext(ctx, `SELECT sequence FROM dna WHERE user = ?`, user).Scan(&sequence); err == sql.ErrNoRows {
+		return "", ErrInvalidUser
+	} else if err != nil {
+		return "", fmt.Errorf("error reading repository: %v ", err)
+	}
+	return sequence, nil
 }
